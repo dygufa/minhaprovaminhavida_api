7bc8255e60b9ee7f69ecd8073621b09c1cac2834
Rodrigo Araujo <dygufa@gmail.com>
2017-12-31 03:33:11 -0300

    adding provider model

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 
-import { File, Course, University, User } from "./models/";
+import { File, Course, University, User, Provider } from "./models/";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL || "");
-sequelize.addModels([File, Course, University, User]);
+sequelize.addModels([File, Course, University, User, Provider]);
 export default sequelize;
