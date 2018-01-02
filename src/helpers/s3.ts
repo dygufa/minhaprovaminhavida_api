d991bc8291563d351ca6357a885fdb4340218ba0
Rodrigo Araujo <dygufa@gmail.com>
2018-01-02 18:32:02 -0300

    propelly typing s3 upload routine

--- src/helpers/s3.ts
+++ src/helpers/s3.ts
@@ -3,33 +3,29 @@ import * as slug from "slug";
 import * as mime from "mime-types";
 import * as fs from "fs";
 import getEnv from "../helpers/env";
+import { ManagedUpload } from "aws-sdk/clients/s3";
 
 const S3_BUCKET = getEnv("S3_BUCKET");
 const s3 = new aws.S3();
 
-// TODO
-export function upload(data: any) {
-    const file = data.file;
+export async function upload(file: Express.Multer.File){
     const filename = slug(file.originalname.replace(/\.[^/.]+$/, ""));
     const extension = mime.extension(file.mimetype);
     const newFilename = Date.now().toString() + '-' + filename + '.' + extension;
 
     const fileStream = fs.createReadStream(file.path);
     
-    return new Promise(function (resolve, reject) {        
-        const params = {
+    return <Promise<ManagedUpload.SendData>> new Promise((resolve, reject) => {     
+        s3.upload({
             Bucket: S3_BUCKET,
             Key: newFilename,
             ContentType: file.mimetype,
             ACL: 'public-read',
             Body: fileStream
-        };
-        
-        s3.upload(params).send((err, data) => {
+        }).send((err, data) => {
             if (err) {
                 return reject(err);
             }
-
             return resolve(data);
         });
     });
